42ddb23c93acdf243500ad7ce74c449e2856fd59
k123s456h <k123s456h@gmail.com>
2019-06-08 00:45:08 +0900

    ^p^

--- whiteboard/js/back/auth.js
+++
@@ -1,4 +0,0 @@
-    // 로그인 "https://auth.korea.ac.kr/directLoginNew.jsp?id="+id+"&pw="+pw+"&returnURL=kulms.korea.ac.kr'
-    // 유저 정보 https://kulms.korea.ac.kr/learn/api/public/v1/users?userName=${학번}
-
-    // 웹 접근 https://developer.chrome.com/extensions/xhr
